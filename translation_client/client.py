8b4bce71b4d8bdf4757453dee7c6d37c9bc57429
Rishi Desai <rishindesai@yahoo.com>
2024-10-24 22:21:42 -0400

    improved clarifications in code documentatiopn

--- translation_client/client.py
+++ translation_client/client.py
@@ -9,22 +9,24 @@ class TranslationClient:
         """
         Initialize the client library with configurable polling and number of retries.
 
-        The purpose of having these variables is for us to have control over how many times we
-        would be calling the "/status" API as per the best appropriate, as opposed to the customer
-        calling it directly and having to deal with the risks associated with it. We want to keep
-        the polling logic abstracted from customer for ease of usage and eliminating the risks associated.
+        The purpose of these variables is to control the frequency of "/status" API calls,
+        optimizing usage on behalf of the customer. By handling the polling logic internally,
+        we abstract it away from the customer, allowing easier usage and reducing the risks they
+        would face if they called the "/status" API directly.
 
         This also gives us room to implement a backoff/jitter mechanism if required.
 
         :param base_url: Base URL of the translation server (e.g., "http://localhost:5000").
         
-        At this time, we are not offering these parameters for customers to modify. This decision is to maintain control over the frequency of server pings and to mitigate any associated risks.
+        At this time, we are not offering these parameters for customers to modify. This decision is to
+        maintain control over the frequency of server pings and to mitigate any associated risks.
+
         initial_polling_interval: Initial interval (in seconds) between status polls.
         max_retries: Maximum number of retries for polling.
         backoff_factor: Multiplier for backoff strategy (increases interval between polls).
         max_interval: Puts a limit on the max interval (in seconds) length when using backoff factor.
 
-        _stop_event: Using it for thread management to have only one thread at a time.
+        _stop_event: Using it for thread management.
         current_thread: Keeps a reference to the thread to use it later for destroying if necessary.
         logger: Reference to the logger.
         """
@@ -46,7 +48,7 @@ class TranslationClient:
     def _poll_status(self):
         """
         Poll the server for the status of the translation job until it has finished running.
-        Implement a backoff mechanism for intervals when we come across potentially (self resolvable or short-lived) errors in fetching the status.
+        Implement a backoff mechanism for intervals when we come across potentially self-resolvable or short-lived errors in fetching the status.
         """
         retries = 0
         curr_interval = self.initial_polling_interval
